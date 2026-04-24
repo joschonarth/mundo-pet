b7f6038de20038861a0f4dd92699ab83d5918bd7
joschonarth <joschonarth@gmail.com>
2026-04-23 22:57:30 -0300

    feat: create home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <div>
-      <h2 className="text-accent-blue">Pet Shop</h2>
+    <div className="bg-background-primary p-6">
+      <div className="flex items-center justify-between md:m-8">
+        <div>
+          <h1 className="mb-2 text-content-primary text-title-size">
+            Sua Agenda
+          </h1>
+          <p className="text-content-secondary text-paragraph-medium-size">
+            Aqui você pode ver todos os clientes e serviços agendados para hoje.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
