7c2c42ffffed426e20a49ee2fb39c5c29d83fc1b
joschonarth <joschonarth@gmail.com>
2026-04-26 22:17:52 -0300

    feat: add calendar date input

--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Dog, Phone, User } from 'lucide-react'
+import { format, startOfToday } from 'date-fns'
+import { CalendarIcon, ChevronDownIcon, Dog, Phone, User } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import { IMaskInput } from 'react-imask'
 import z from 'zod'
 import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
 import {
   Dialog,
   DialogContent,
@@ -23,13 +25,26 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 import { Textarea } from '@/components/ui/textarea'
+import { cn } from '@/lib/utils'
 
 const appointmentFormSchema = z.object({
   tutorName: z.string().min(3, 'O nome do tutor é obrigatório'),
   petName: z.string().min(3, 'O nome do pet é obrigatório'),
   phone: z.string().min(11, 'O telefone é obrigatório'),
   description: z.string().min(3, 'A descrição é obrigatória'),
+  scheduleAt: z
+    .date({
+      error: 'A data é obrigatória',
+    })
+    .min(startOfToday(), {
+      message: 'A data não pode ser no passado',
+    }),
 })
 
 type AppointFormValues = z.infer<typeof appointmentFormSchema>
@@ -42,6 +57,7 @@ export const AppointmentForm = () => {
       petName: '',
       phone: '',
       description: '',
+      scheduleAt: undefined,
     },
   })
 
@@ -163,6 +179,53 @@ export const AppointmentForm = () => {
                       {...field}
                     />
                   </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="scheduleAt"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel className="text-content-primary text-label-medium-size">
+                    Data
+                  </FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          className={cn(
+                            'w-full justify-between border-border-primary bg-background-tertiary text-left font-normal text-content-primary hover:border-border-secondary hover:bg-background-tertiary hover:text-content-primary focus:border-border-brand focus-visible:border-border-brand focus-visible:ring-1 focus-visible:ring-border-brand focus-visible:ring-offset-0',
+                            !field.value && 'text-content-secondary'
+                          )}
+                          variant="outline"
+                        >
+                          <div className="flex items-center gap-2">
+                            <CalendarIcon
+                              className="text-content-brand"
+                              size={20}
+                            />
+                            {field.value ? (
+                              format(field.value, 'dd/MM/yyyy')
+                            ) : (
+                              <span>Selecione uma data</span>
+                            )}
+                          </div>
+                          <ChevronDownIcon className="h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent align="start" className="w-auto p-0">
+                      <Calendar
+                        disabled={(date) => date < startOfToday()}
+                        mode="single"
+                        onSelect={field.onChange}
+                        selected={field.value}
+                      />
+                    </PopoverContent>
+                  </Popover>
                   <FormMessage />
                 </FormItem>
               )}
