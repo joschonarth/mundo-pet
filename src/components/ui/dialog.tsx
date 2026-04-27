da0c352dd81348328897574be7ea019175cc3a5b
joschonarth <joschonarth@gmail.com>
2026-04-26 22:56:15 -0300

    style: hide scrollbar in dialog component

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -64,7 +64,7 @@ function DialogOverlay({
 }
 
 const dialogContentVariants = cva(
-  'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 p-6 shadow-lg duration-200 data-[state=closed]:animate-out data-[state=open]:animate-in',
+  'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 scrollbar-hidden fixed top-[50%] left-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 p-6 shadow-lg duration-200 data-[state=closed]:animate-out data-[state=open]:animate-in',
   {
     variants: {
       variant: {
